fe66460b44324b97902366dda456903a6a0c2e8c
piet <pprotassow@posteo.de>
2024-09-24 12:01:04 +0200

    admin forms & post model & Test

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff"
+    ]
+    fieldsets = UserAdmin.fieldsets + (('my_name_label', {"fields": ("name",)}),)
+    # fieldsets = (('my_name_label', {"fields": ("name", "password")}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (('my_name_label', {"fields": ("name",)}),)
+    # add_fieldsets =  (('my_name_label', {"fields": ("name",)}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
